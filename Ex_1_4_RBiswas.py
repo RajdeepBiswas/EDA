577de227247287151f6fa6fbc351661fc89b2ba4
Rajdeep Biswas <clickrajdeep@gmail.com>
2020-05-31 18:24:48 -0500

    first commit

---
+++ Ex_1_4_RBiswas.py
@@ -0,0 +1,46 @@
+# -------------------------------------------------------------------------
+# File: Ex_1_4_RBiswas.py
+# Name: Rajdeep Biswas
+# Date: 05/31/2020
+# Desc: EDA Exercise 1.4 for the following tasks
+#Display the text “Hello World! I wonder why that is always the default coding text to start with”
+#Add two numbers together
+#Subtract a number from another number
+#Multiply two numbers
+#Divide between two numbers
+#Concatenate two strings together (any words)
+#Create a list of 4 items (can be strings, numbers, both)
+#Append  an item to your list (again, can be a string, number)
+#Create a tuple with 4 items (can be strings, numbers, both)
+# -------------------------------------------------------------------------
+
+#Display the text “Hello World! I wonder why that is always the default coding text to start with”
+print('“Hello World! I wonder why that is always the default coding text to start with”')
+
+#Add two numbers together
+print ('1+2=',1+2)
+
+#Subtract a number from another number
+print ('2-1=',2-1)
+
+#Multiply two numbers
+print ('2*1.6=',2*1.6)
+
+#Divide between two numbers
+print ('2.5/2=',2.5*2)
+
+#Concatenate two strings together (any words)
+print('two','strings', sep=' ')
+
+#Create a list of 4 items (can be strings, numbers, both)
+my_list = [1,2.5,'a','xyz']
+#print(my_list[0:])
+print ('my_list: ',my_list)
+
+#Append an item to your list (again, can be a string, number)
+my_list.append('new')
+print ('my_list: ',my_list)
+
+#Create a tuple with 4 items (can be strings, numbers, both)
+my_tupple = (1,2.5,'a','xyz')
+print('my_tupple: ',my_tupple)
